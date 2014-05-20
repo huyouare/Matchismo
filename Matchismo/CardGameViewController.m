d7e5c0ee782fa67caf383eba29ae600789be89c7
Jesse Hu <huyouare@gmail.com>
2014-05-19 20:09:31 -0400

    Added card flipping button

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -14,7 +14,17 @@
 
 @implementation CardGameViewController
 
+- (IBAction)touchCardButton:(UIButton *)sender {
+    if ([sender.currentTitle length]) {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardback"] forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+    } else {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                forState:UIControlStateNormal];
+        [sender setTitle:@"A♣︎" forState:UIControlStateNormal];
+    }
 
+}
 
 //- (void)viewDidLoad
 //{
