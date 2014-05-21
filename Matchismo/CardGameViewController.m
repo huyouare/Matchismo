247ddc92950f385a91f30cfa542b9e42015ac3db
Jesse Hu <huyouare@gmail.com>
2014-05-21 00:14:57 -0400

    Added UpdateUI logic

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -41,7 +41,23 @@
 
 - (void)updateUI
 {
-    
+    for (UIButton *cardButton in self.cardButtons) {
+        int cardButtonIndex = [self.cardButtons indexOfObject:cardButton];
+        Card *card = [self.game cardAtIndex:cardButtonIndex];
+        [cardButton setTitle:[self titleForCard:card] forState:UIControlStateNormal];
+        [cardButton setImage:[self backgroundImageForCard:card] forState:UIControlStateNormal];
+        cardButton.enabled = card.isMatched;
+    }
+}
+
+- (NSString *)titleForCard:(Card *)card
+{
+    return card.isChosen ? card.contents : @"";
+}
+
+- (UIImage *)backgroundImageForCard:(Card *)card
+{
+    return [UIImage imageNamed:card.isChosen ? @"cardfront" : @"cardback"];
 }
 
 //- (void)viewDidLoad
