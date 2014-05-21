183d0b39d87009f0312d85a444ab8b5bc6116d4b
Jesse Hu <huyouare@gmail.com>
2014-05-20 23:41:28 -0400

    Changed scoring and added matching

--- Matchismo/Model/CardMatchingGame.m
+++ Matchismo/Model/CardMatchingGame.m
@@ -15,6 +15,10 @@
 
 @implementation CardMatchingGame
 
+static const int MISMATCH_PENALTY = 2;
+static const int MATCH_BONUS = 4;
+static const int COST_TO_CHOOSE = 1;
+
 - (NSMutableArray *)cards
 {
     if (!_cards) {
@@ -41,9 +45,33 @@
     }
     return self;
 }
+
 - (void)chooseCardAtIndex:(NSUInteger)index
 {
-    Card
+    Card *card = [self cardAtIndex:index];
+    if (!card.isMatched) {
+        if (card.isChosen) {
+            card.chosen = NO;
+        } else {
+            //match against other cards
+            for (Card * otherCard in self.cards) {
+                if (otherCard.isChosen && !otherCard.isMatched) {
+                    int matchScore = [card match:@[otherCard]];
+                    if (matchScore) {
+                        self.score += matchScore * MATCH_BONUS;
+                        card.matched = YES;
+                        otherCard.matched = YES;
+                    } else {
+                        self.score -= MISMATCH_PENALTY;
+                        otherCard.chosen = NO;
+                        break; //only 2 for now
+                    }
+                }
+                self.score -= COST_TO_CHOOSE;
+                card.chosen = YES;
+            }
+        }
+    }
 }
 - (Card *)cardAtIndex:(NSUInteger)index
 {
